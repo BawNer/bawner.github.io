5b4010edbeeab29a74aae4139fe42f54c3cd0181
BawNer <aleksandr.krasnikov2000@gmail.com>
2020-04-14 23:32:49 +0300

    skelet

---
+++ skf/precache-manifest.3aa1a56b20d82bb686a0468e3d4cd91c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdbb0b6dd21dcf6344f",
+    "url": "/skf/css/app.7759ca10.css"
+  },
+  {
+    "revision": "cf908d487b0a16c8e5e7",
+    "url": "/skf/css/chunk-vendors.673ce51a.css"
+  },
+  {
+    "revision": "35bebbc78749928398309f93e30969e0",
+    "url": "/skf/index.html"
+  },
+  {
+    "revision": "dcdbb0b6dd21dcf6344f",
+    "url": "/skf/js/app.6ae3b26e.js"
+  },
+  {
+    "revision": "cf908d487b0a16c8e5e7",
+    "url": "/skf/js/chunk-vendors.2f49b865.js"
+  },
+  {
+    "revision": "3195843f7f3e0915d0f7c38b0036054a",
+    "url": "/skf/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/skf/robots.txt"
+  }
+]);
