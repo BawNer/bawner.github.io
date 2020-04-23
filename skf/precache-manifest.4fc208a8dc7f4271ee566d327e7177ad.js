c3d859f47fbfe1e68554d1135b2f96f5a7132cab
BawNer <aleksandr.krasnikov2000@gmail.com>
2020-04-23 23:13:04 +0300

    bug fixed

--- skf/precache-manifest.4fc208a8dc7f4271ee566d327e7177ad.js
+++ skf/precache-manifest.4fc208a8dc7f4271ee566d327e7177ad.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2eb1c73ef906e96e54c1",
+    "revision": "421540d4af6cd369444b",
     "url": "/skf/css/app.7759ca10.css"
   },
   {
-    "revision": "ab4fa6dd115bdc37c1a2",
-    "url": "/skf/css/chunk-vendors.f493d424.css"
+    "revision": "c5e78d21e0bdb431fbb5",
+    "url": "/skf/css/chunk-vendors.8db6652f.css"
   },
   {
-    "revision": "bdd39ee3243c2c8073ba10da640b254f",
+    "revision": "64962943ef138fc5ef93d0e2fb1d14a6",
     "url": "/skf/index.html"
   },
   {
-    "revision": "2eb1c73ef906e96e54c1",
-    "url": "/skf/js/app.fba7a3b4.js"
+    "revision": "421540d4af6cd369444b",
+    "url": "/skf/js/app.eb8858d7.js"
   },
   {
     "revision": "0d17ee8f7b703565c720",
     "url": "/skf/js/chunk-ed317e18.77f6edfa.js"
   },
   {
-    "revision": "ab4fa6dd115bdc37c1a2",
-    "url": "/skf/js/chunk-vendors.609d45ee.js"
+    "revision": "c5e78d21e0bdb431fbb5",
+    "url": "/skf/js/chunk-vendors.b21daa3e.js"
   },
   {
     "revision": "3195843f7f3e0915d0f7c38b0036054a",
