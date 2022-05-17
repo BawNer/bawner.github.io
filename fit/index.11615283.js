231b00f484d0b31dfc3fa3c9d55ae57405506579
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-05-18 00:09:40 +0300

    add modal

---
+++ fit/index.11615283.js
@@ -0,0 +1,2 @@
+window.onload=()=>{setTimeout((()=>{document.getElementById("preloader").style.display="none"}),500)};const modal=e=>{const o=document.getElementById("modal");o.style.display=e?"flex":"none"};
+//# sourceMappingURL=index.11615283.js.map
--- fit/index.22e07a97.js
+++
@@ -1,2 +0,0 @@
-window.onload=()=>{setTimeout((()=>{document.getElementById("preloader").style.display="none"}),500)};
-//# sourceMappingURL=index.22e07a97.js.map
